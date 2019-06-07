977df7019b67b64b7a72ad58ce9c97876c1872cd
Erica Elliston <erica.elliston06@myhunter.cuny.edu>
2019-06-07 15:53:27 -0400

    modfies file organization

--- src/components/Campuses.js
+++ src/components/Campuses.js
@@ -3,14 +3,18 @@ import NavBar from './navbar.js'
 import './App.css';
 
 
-/*const campusView = (props)=>{
+const campusView = (props)=>{
     {name, numberOfStudents, images} = props;
     return(
       <div>
-          <div className="campus-icon"></div>
+          <div className="campus-icon">{image}</div>
+          <div className="campus-name">{name}</div>
+          <div className="numOfStud">{numberOfStudents}</div>
       </div>
     )
-  }*/
+
+}
+  
   
 
 class Campuses extends React.Component{
@@ -21,13 +25,20 @@ class Campuses extends React.Component{
         }
     }
     render(){
+            
+            const singleCampus= this.state.campuses.map(campus=>{
+               return(<campusView  name={campus.name}
+                numberOfStudents={campus.name}
+                image={campus.image}
+            />)
+            });
         return(
             <div>
                 <header>
                      <h1>Campuses</h1>
                      <br />
                      <NavBar />  
-        {this.props.campuses.length === 0? <h3>No Campuses to Display</h3> : <div></div> } 
+                    {this.props.campuses.length === 0? <h3>No Campuses to Display</h3> : <div>{singleCampus}</div> } 
                 </header>
             </div>
         )
